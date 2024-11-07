90d7417cc9b30f87d712fc897af605fbdc75c6b7
meesa susmith <45920678+meesasusmith@users.noreply.github.com>
2024-11-06 20:27:23 -0600

    Initial Commit

---
+++ RetirementPlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetirementPlan
+//
+//  Created by Erikneon on 11/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
